17c8ac43d10d9fe1c5f0022410a4933cf1f82b44
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-21 12:57:07 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,20 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q okteta | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/128x128/apps/okteta.png
+export DESKTOP=/usr/share/applications/org.kde.okteta.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/okteta     \
+	/usr/bin/struct2osd \
+	/usr/lib/*Okteta*   \
+	/usr/share/okteta
 
 # Additional changes can be done in between here
 
